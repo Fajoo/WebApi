421e266eebb7f2d02500056082f8c33b7e78f4cd
Fajo <dimasav57@gmail.com>
2022-01-11 20:24:43 +0300

    DI added

---
+++ WebApi.Persistence/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApi.Application.Interfaces;
+
+namespace WebApi.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection
+        services, IConfiguration configuration)
+    {
+        var connectionString = configuration["DbConnection"];
+        services.AddDbContext<WebApiDbContext>(options =>
+        {
+            options.UseSqlite(connectionString);
+        });
+        services.AddScoped<IWebApiDbContext>(provider =>
+            provider.GetService<WebApiDbContext>());
+        return services;
+    }
+}
