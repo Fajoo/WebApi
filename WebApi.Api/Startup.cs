781b67f6287751eaf255e5b75f7effca8d61cb77
Fajo <dimasav57@gmail.com>
2022-01-11 19:28:46 +0300

    Application architecture creation

---
+++ WebApi.Api/Startup.cs
@@ -0,0 +1,25 @@
+﻿namespace WebApi.Api;
+
+public class Startup
+{
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
---
+++ WebApi.Domain/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi.Domain;
+
+public class Person
+{
+    public Guid Id { get; set; }
+    public string FIO { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
